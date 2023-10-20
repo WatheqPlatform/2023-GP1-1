ddfbfb9ad1fc5acef61c52daea79b122089f4119
RagadAlhindi <128581821+RagadAlhindi@users.noreply.github.com>
2023-10-20 16:52:45 +0300

    Add files via upload

--- Functions/Logout.js
+++ Functions/Logout.js
@@ -4,16 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Add a click event listener to the logout button
     logoutButton.addEventListener("click", function() {
-        // Display a confirmation dialog
-        var confirmLogout = confirm("Are you sure you want to logout?");
-        
-        // Check the user's choice
-        if (confirmLogout) {
-            // If the user confirms, redirect to the logout page or perform logout actions
+       
             window.location.href = "../index.php"; 
-        } else {
-            // If the user cancels, do nothing or stay on the current page
-        }
+        
     });
 });
 

2bb6b496ae83e561fe825f8664f028702800ee0a
RagadAlhindi <128581821+RagadAlhindi@users.noreply.github.com>
2023-10-24 21:15:39 +0300

    Add files via upload

--- AddOffer Page/Validation.js
+++ AddOffer Page/Validation.js
@@ -37,79 +37,78 @@ $(document).ready(function () {
             });
 
         } else {
-            
-                    if (parseFloat($("#minSalary").val()) > parseFloat($("#maxSalary").val()))
-                    {
-                        alert("Minimum salary cannot be greater than the maximum salary.");
 
-                    } else {
+            if (parseFloat($("#minSalary").val()) > parseFloat($("#maxSalary").val()))
+            {
+                alert("Minimum salary cannot be greater than the maximum salary.");
+
+            } else {
 
 
 
-                        var skills = getSkills();
+                var skills = getSkills();
 
 
 // Call the saveQualifications() function and log the returned data
-                        var qualifications = saveQualifications();
-
+                var qualifications = saveQualifications();
+                // check that if there's at least one feild filled, the other should be filled too
+                var checkQualification = checkQualifications(qualifications);
 
 // Call the saveExperiences() function and log the returned data
-                        var experiences = saveExperiences();
-                        if (experiences === false) {
-                            alert("Please enter a valid number for Experience years.");
-                        } else {
-                            //Send the information to PHP File
-                            $.post("AddOfferLogic.php", {
-                                jobTitle: inputValues[0],
-                                jobDescription: inputValues[1],
-                                jobField: inputValues[2],
-                                jobAddress: inputValues[3],
-                                jobType: inputValues[4],
-                                minSalary: inputValues[5],
-                                maxSalary: inputValues[6],
-                                jobCategories: inputValues[7],
-                                jobCity: inputValues[8],
+                var experiences = saveExperiences();
+                // check that if there's at least one feild filled, the other should be filled too
+                var checkExperiences = checkExperience(experiences);
+                if (checkQualification === true && checkExperiences === true) {
 
-                                // non required feilds we won't check if they are empty 
-                                startingDate: $("#date").val(),
-                                workingHours: $("#workingHours").val(),
-                                notes: $("#notes").val(),
-                                workingDays: getWorkingDays(),
-                                //skills: getSkills(),
-                                // qualifications: saveQualifications(),
-                                //s experiences: saveExperiences(),
-                                skills: skills,
-                                qualifications: qualifications,
-                                experiences: experiences
+                    //Send the information to PHP File
+                    $.post("AddOfferLogic.php", {
+                        jobTitle: inputValues[0],
+                        jobDescription: inputValues[1],
+                        jobField: inputValues[2],
+                        jobAddress: inputValues[3],
+                        jobType: inputValues[4],
+                        minSalary: inputValues[5],
+                        maxSalary: inputValues[6],
+                        jobCategories: inputValues[7],
+                        jobCity: inputValues[8],
 
-                            }, function (data) {
+                        // non required feilds we won't check if they are empty 
+                        startingDate: $("#date").val(),
+                        workingHours: $("#workingHours").val(),
+                        notes: $("#notes").val(),
+                        workingDays: getWorkingDays(),
 
-                                if (data === "success") { // Update the condition to trim the data
-                                    $('#AddForm')[0].reset(); // Delete information from the form
-                                    var modal_wrapper = document.querySelector(".modal_wrapper");
-                                    var success_wrap = document.querySelector(".success_wrap");
-                                    var shadow = document.querySelector(".shadow");
+                        skills: skills,
+                        qualifications: qualifications,
+                        experiences: experiences
 
-                                    modal_wrapper.classList.add("active");
-                                    success_wrap.classList.add("active");
+                    }, function (data) {
 
-                                    // Clicking anywhere on the screen remove the message
-                                    shadow.addEventListener("click", function () {
-                                        modal_wrapper.classList.remove("active");
-                                        success_wrap.classList.remove("active");
-                                        window.location.href = "../History Page/History.php";
-                                    });
-                                } else {
-                                    // Handle error case
-                                    alert("An error occurred during form submission. Please try again");
-                                    console.error("Form submission error:", data);
-                                }
+                        if (data === "success") { // Update the condition to trim the data
+                            $('#AddForm')[0].reset(); // Delete information from the form
+                            var modal_wrapper = document.querySelector(".modal_wrapper");
+                            var success_wrap = document.querySelector(".success_wrap");
+                            var shadow = document.querySelector(".shadow");
 
+                            modal_wrapper.classList.add("active");
+                            success_wrap.classList.add("active");
+
+                            // Clicking anywhere on the screen remove the message
+                            shadow.addEventListener("click", function () {
+                                modal_wrapper.classList.remove("active");
+                                success_wrap.classList.remove("active");
+                                window.location.href = "../History Page/History.php";
                             });
+                        } else {
+                            // Handle error case
+                            alert("An error occurred during form submission. Please try again");
+                            console.error("Form submission error:", data);
                         }
-                    
+
+                    });
                 }
-            
+
+            }
 
 
             function getWorkingDays() {
@@ -134,6 +133,24 @@ $(document).ready(function () {
                 return skills;
             }
 
+            function checkQualifications(checkQualifications) {
+                if (checkQualifications === null)
+                {
+                    return true;
+                }
+                for (var i = 0; i < qualifications.length; i++) {
+                    var qualification = qualifications[i];
+                    if (!qualification.level && qualification.field) {
+                        alert("Qualification degree level is missiing.");
+                        return false;
+                    } else if (qualification.level && !qualification.field) {
+                        alert("Qualification degree field is missing.");
+                        return false;
+                    }
+                }
+                return true;
+            }
+
             function saveQualifications() {
                 var qualifications = [];
 
@@ -141,57 +158,72 @@ $(document).ready(function () {
                     var degreeLevel = $(this).find("select[name^='degreeLevel']").val();
                     var degreeField = $(this).find("input[name^='degree']").val();
 
-                    if (degreeLevel && degreeField) {
-                        degreeLevel = degreeLevel.trim();
-                        degreeField = degreeField.trim();
 
-                        if (degreeLevel !== "" && degreeField !== "") {
-                            // Check if the qualification already exists in the qualifications array
-                            var exists = qualifications.some(function (qualification) {
-                                return qualification.level === degreeLevel && qualification.field === degreeField;
-                            });
 
-                            if (!exists) {
-                                qualifications.push({
-                                    level: degreeLevel,
-                                    field: degreeField
-                                });
-                            }
-                        }
+                    var existingQualification = qualifications.find(function (qualification) {
+                        return qualification.level === degreeLevel && qualification.field === degreeField;
+                    });
+
+                    if (!existingQualification && (degreeLevel !== null || degreeField !== ""))
+                    {
+                        qualifications.push({
+                            level: degreeLevel,
+                            field: degreeField
+                        });
                     }
                 });
 
+
+
                 return qualifications;
             }
+            function checkExperience(experience) {
+                if (experience === null)
+                {
+                    return true;
+                }
 
+                for (var i = 0; i < experience.length; i++)
+                {
+                    var ex = experience[i];
+                    if (!ex.field) {
+                        alert("Experience field is missing.");
+                        return false;
+                    } else if (!ex.description) {
+                        alert("Experience description is missing.");
+                        return false;
+                    } else if (!ex.years) {
+                        alert("Experience years is missing.");
+                        return false;
+                    }
+                }
+                return true;
+            }
 
             function saveExperiences() {
                 var experiences = [];
 
                 $("div[id^='experience']").each(function () {
-                    var field = $(this).find("input[name^='experience']").eq(0).val().trim();
-                    var description = $(this).find("input[name^='experiences']").eq(0).val().trim();
+                    var field = $(this).find("input[name^='experience']").eq(0).val();
+                    var description = $(this).find("input[name^='experiences']").eq(0).val();
                     var years = $(this).find("input[name^='experiences']").eq(1).val();
 
-                    var YearisInteger = Number.isInteger(parseInt(years));
 
-                    if (field !== "" && description !== "" && years !== "" && YearisInteger === true) {
-                        // Check if the experience already exists in the experiences array
-                        var exists = experiences.some(function (experience) {
-                            return experience.field === field && experience.description === description && experience.years === years;
+
+
+                    // Check if the experience already exists in the experiences array
+                    var exists = experiences.some(function (experience) {
+                        return experience.field === field && experience.description === description && experience.years === years;
+                    });
+
+                    if (!exists && (field !== "" || description !== "" || years !== "")) {
+                        experiences.push({
+                            field: field,
+                            description: description,
+                            years: years
                         });
-
-                        if (!exists) {
-                            experiences.push({
-                                field: field,
-                                description: description,
-                                years: years
-                            });
-                        }
-                    } else {
-
-                        return false;
                     }
+
                 });
 
                 return experiences;
@@ -201,7 +233,6 @@ $(document).ready(function () {
     });
 
 });
-
 
 
 

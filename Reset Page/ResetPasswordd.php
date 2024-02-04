6522c76c390c1d66e0e0b43464a4990cb64ebf90
RagadAlhindi <128581821+RagadAlhindi@users.noreply.github.com>
2024-02-04 21:10:49 +0300

    Update ResetPasswordd.php

--- Reset Page/ResetPasswordd.php
+++ Reset Page/ResetPasswordd.php
@@ -98,7 +98,7 @@
     <script src="https://kit.fontawesome.com/cc933efecf.js" crossorigin="anonymous"></script> <!--Icons retrevial-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://kit.fontawesome.com/cc933efecf.js" crossorigin="anonymous"></script> <!--Icons retrevial-->
-    <script src="../Functions/ValidatePassword.js"></script>
+    <script src="../Functions/PasswordValidate.js"></script>
     <script src="ResetValidation.js"></script>
    
 </head>
